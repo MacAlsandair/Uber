48570088c8e5fb7487f730d57de8bb855eb1eef5
ILUHA <bkmrfrbkmrf326915@mail.ru>
2022-08-29 12:33:20 +0200

    uploading project to GitHub

---
+++ js/script.js
@@ -0,0 +1,30 @@
+// Выдвигает менюшку при нажатии на гамбургер для маленьких устройств
+window.addEventListener('DOMContentLoaded', () => {
+    const menu = document.querySelector('.menu'),
+    menuItem = document.querySelectorAll('.menu_item'),
+    hamburger = document.querySelector('.hamburger');
+
+    hamburger.addEventListener('click', () => {
+        hamburger.classList.toggle('hamburger_active');
+        menu.classList.toggle('menu_active');
+    });
+
+    menuItem.forEach(item => {
+        item.addEventListener('click', () => {
+            hamburger.classList.toggle('hamburger_active');
+            menu.classList.toggle('menu_active');
+        })
+    })
+})
+
+// Скрывает меню при прокрутке вниз и проявляет при прокрутке вверх
+var prevScrollpos = window.pageYOffset;
+window.onscroll = function() {
+  var currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-50px";
+  }
+  prevScrollpos = currentScrollPos;
+}
